e738618e1b3ae4a5f1c9d349c0d48e0727449585
ALEXANDRA ENCARNACION <alex1102e@gmail.com>
2024-09-23 18:37:08 +0100

    Modify signUpForm

--- secret_santa_wishlist/secretsanta/forms.py
+++ secret_santa_wishlist/secretsanta/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from .models import User
 
-class SignUpForm(forms.Form):
-    first_name = forms.CharField(label='First name', max_length=50)
-    last_name = forms.CharField(label='Last name', max_length=50)
-    username = forms.CharField(label='Username', max_length=30)
-    email = forms.EmailField(label='Email')
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
+
     new_password = forms.CharField(label='Password', widget=forms.PasswordInput())
     password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
