7b02245168c6f9dd6352b06972ddf0ee875a16f9
ALEXANDRA ENCARNACION <alex1102e@gmail.com>
2024-09-26 23:34:53 +0100

    Implement messages partial

--- secret_santa_wishlist/secret_santa_wishlist/settings.py
+++ secret_santa_wishlist/secret_santa_wishlist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'secretsanta.User'
+
+MESSAGE_TAGS = {
+    message_constants.DEBUG: 'dark',
+    message_constants.ERROR: 'danger',
+}
