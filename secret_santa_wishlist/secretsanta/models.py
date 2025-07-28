aff37db2e64973dc8ce47575cce6d1375faf188c
ALEXANDRA ENCARNACION <alex1102e@gmail.com>
2025-07-28 16:37:35 +0100

    Add grup type in group model

--- secret_santa_wishlist/secretsanta/models.py
+++ secret_santa_wishlist/secretsanta/models.py
@@ -10,14 +10,23 @@ class User(AbstractUser):
 
 # Group Model
 class Group(models.Model):
+    GROUP_TYPE_CHOICES = [
+        ('family_friends', 'Family & Friends'),
+        ('work', 'Work'),
+    ]
+
     name = models.CharField(max_length=100, unique=False)  # Group name
     code = models.CharField(max_length=8, unique=True, blank=False)  # Random unique code
     # users = models.ManyToManyField('User', related_name='user_groups')  # Group members
+    group_type = models.CharField(max_length=20, choices=GROUP_TYPE_CHOICES, default='family_friends')
     admin = models.ForeignKey(User, related_name='admin_groups', on_delete=models.CASCADE)  # Admin who created the group
 
+    def __str__(self):
+        return f"{self.name} ({self.get_group_type_display()})"
+    
 class GroupMember(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='group_memberships')  # Links to User model
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='members')  # Links to Group model
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Links to User model
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)  # Links to Group model
     date_joined = models.DateTimeField(auto_now_add=True)  # When the user joined the group
     is_admin = models.BooleanField(default=False)  # Whether the user is an admin of the group
 
