4af9c38ed81684f320f2086a56a3f48839d61fb6
ALEXANDRA ENCARNACION <alex1102e@gmail.com>
2026-04-22 19:53:52 +0100

    Modify wishlist model to have published boolean

--- secret_santa_wishlist/secretsanta/models/wishlist_model.py
+++ secret_santa_wishlist/secretsanta/models/wishlist_model.py
@@ -6,6 +6,8 @@ from .group_model import Group
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlists")
     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="wishlists")
+    published = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
